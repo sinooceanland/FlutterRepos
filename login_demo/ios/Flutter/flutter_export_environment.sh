a235c47c868e8fa45fff785a6654929a916601d1
GJS <1353990812@qq.com>
2019-11-21 13:48:27 +0800

    dio: ^3.0.5

---
+++ login_demo/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Volumes/MacintoshHD/gjs/package/flutter"
+export "FLUTTER_APPLICATION_PATH=/Volumes/MacintoshHD/gjs/文稿/GitWorkSpace/GitHub/Testing/ksti/Flutter/FlutterRepos/login_demo"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Volumes/MacintoshHD/gjs/package/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
